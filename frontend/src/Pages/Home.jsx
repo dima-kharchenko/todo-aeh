f2eaec2e67933883ae23acced75ca16cf40e2d71
dima-kharchenko <dmitriy020606@gmail.com>
2025-05-22 01:02:41 +0200

    Separate new task component

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,36 +1,13 @@
-import { createTask } from "../api";
 import Header from "../Components/Header";
-import { useState } from "react";
+import NewTask from "../Components/NewTask";
 function Home() {
-    const [newTask, setNewTask] = useState("")
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        if(newTask){
-            console.log(newTask)
-            const res = await createTask(newTask, null, null)
-            console.log(res)
-            setNewTask("")
-        }
-    }
 
     return(
     <>
     <Header />
     <div className="mt-12 text-white">
         <div className="pt-4 w-2/3 mx-auto text-center">
-            <form onSubmit={handleSubmit} className="flex w-3/4 mx-auto">
-                <input 
-                    type="text"
-                    id="newTask"
-                    name="newTask"
-                    value={newTask}
-                    onChange={(e) => setNewTask(e.target.value)}
-                    placeholder="Add new task..."
-                    className="bg-surface-a10 px-4 py-2 w-full rounded-lg focus:outline-none focus:ring-2 hover:ring-2 ring-primary-a0 transition"
-                />
-                <button type="submit" className="ml-4 bg-primary-a0 px-4 py-2 rounded-lg cursor-pointer ring-primary-a0 hover:ring-2 active:ring-0 transition">Add</button>
-            </form>
+        <NewTask />
         </div>
     </div>
     </>
