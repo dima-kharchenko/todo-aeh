7a8f913dac45af7c1cf467f520024f57cf55329d
dima-kharchenko <dmitriy020606@gmail.com>
2025-05-23 20:31:02 +0200

    Add category selection & addition

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -5,6 +5,7 @@ import { getTasks, updateTask } from "../api";
 function Home() {
     const [tasks, setTasks] = useState([])
     const [categories, setCategories] = useState([])
+    const [newCategory, setNewCategory] = useState('')
 
     const [hoveredPriority, setHoveredPriority] = useState({});
 
@@ -49,16 +50,39 @@ function Home() {
         const updatedTask = updatedTasks.find(task => task.id === id)
         await updateTask(updatedTask)
     }
-    const handlePriorityChange = async (id, priority) => {
+
+    const handlePriority = async (id, priority) => {
+        const task = tasks.find(t => t.id === id)
+        const newPriority = task.priority === priority ? 0 : priority
+
+        const updatedTasks = tasks.map(task => task.id === id ? { ...task, priority: newPriority } : task)
+        setTasks(updatedTasks)
+
+        const updatedTask = updatedTasks.find(t => t.id === id)
+        await updateTask(updatedTask)
+    }
+
+    const handleCategory = async (id, category) => {
         const task = tasks.find(t => t.id === id);
-        const newPriority = task.priority === priority ? 0 : priority;
+        const newCategory = task.category === category ? 0 : category
 
-        const updatedTasks = tasks.map(task => task.id === id ? { ...task, priority: newPriority } : task);
-        setTasks(updatedTasks);
+        const updatedTasks = tasks.map(task => task.id === id ? { ...task, category: newCategory} : task)
+        setTasks(updatedTasks)
 
-        const updatedTask = updatedTasks.find(t => t.id === id);
-        await updateTask(updatedTask);
-    };
+        const updatedTask = updatedTasks.find(t => t.id === id)
+        await updateTask(updatedTask)
+    } 
+
+    const handleAddCategory = async (e, id) => {
+        e.preventDefault()
+        setCategories(p => ([...p, newCategory]))
+        const updatedTasks = tasks.map(task => task.id === id ? { ...task, category: newCategory} : task)
+        setNewCategory('')
+        setTasks(updatedTasks)
+
+        const updatedTask = updatedTasks.find(t => t.id === id)
+        await updateTask(updatedTask)
+    }
 
     const toggleDropdown = (id) => {
         setDropdownOpen(prev => ({
@@ -94,16 +118,18 @@ function Home() {
                         >
                             <ul className="text-surface-a50 select-none">
                             {categories.map((item, index) =>
-                                <li key={index} className={`my-2 mx-2 py-1 px-3 text-sm rounded-lg hover:text-white cursor-pointer transition ${task.category === item ? 'bg-primary-a0 text-white' : 'bg-surface-a20 '}`}>
+                                <li key={index} onClick={() => handleCategory(task.id, item)} className={`my-2 mx-2 py-1 px-3 text-sm rounded-lg hover:text-white cursor-pointer transition ${task.category === item ? 'bg-primary-a0 text-white' : 'bg-surface-a20 '}`}>
                                 {item} 
                                 </li>
                             )}
                                 <li key="new-collection" className="mx-2">
-                                    <form>
+                                    <form onSubmit={(e) => handleAddCategory(e, task.id)}>
                                         <input
                                             placeholder="Add"
                                             className="mb-2 px-2 py-1 w-full rounded-lg text-center focus:outline-none field-sizing-content bg-surface-a20 placeholder:text-surface-a50 text-sm focus:placeholder:text-transparent transition"
                                             type="text"
+                                            value={newCategory}
+                                            onChange={(e) => setNewCategory(e.target.value)}
                                             />
                                     </form>
                                 </li>
@@ -155,7 +181,7 @@ function Home() {
                                     return updated
                                   })
                                 }
-                                onClick={!task.done ? () => handlePriorityChange(task.id, priority) : ''}
+                                onClick={!task.done ? () => handlePriority(task.id, priority) : ''}
                                 className={`w-3 h-3 rounded-sm transition ${
                                   active && !task.done ? "bg-primary-a30 cursor-pointer" : "bg-surface-a20"
                                 }`}
