77838ff09e0f1de49d344f0a535c158b083a42e0
dima-kharchenko <dmitriy020606@gmail.com>
2025-05-23 21:56:03 +0200

    Separate dropdown categories

---
+++ frontend/src/Components/DropdownCategories.jsx
@@ -0,0 +1,96 @@
+import { useState, useEffect, useRef } from "react"
+import { getTasks, updateTask } from "../api"
+
+function DropdownCategories({tasks, task, setTasks}){
+    const [categories, setCategories] = useState([])
+    const [newCategory, setNewCategory] = useState('')
+    const [dropdownOpen, setDropdownOpen] = useState(null)
+    const dropdownRef = useRef(null)
+    const buttonRef = useRef(null);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                let data = await getTasks()
+                data = data.filter(task => !task.done).sort((a, b) => b.id - a.id).sort((a, b) => a.done - b.done)
+                setCategories([...new Set(data.map(task => task.category).filter(Boolean))])
+            } catch(err) {
+                console.log(err)
+            }
+        })()
+    }, [])
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (
+                !event.target.closest(".dropdown") &&
+                !event.target.closest(".dropdown-button")
+            ) {
+                setDropdownOpen(null);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    const handleCategory = async (id, category) => {
+        const task = tasks.find(t => t.id === id);
+        const newCategory = task.category === category ? 0 : category
+
+        const updatedTasks = tasks.map(task => task.id === id ? { ...task, category: newCategory} : task)
+        setTasks(updatedTasks)
+
+        const updatedTask = updatedTasks.find(t => t.id === id)
+        await updateTask(updatedTask)
+    } 
+
+    const handleAddCategory = async (e, id) => {
+        e.preventDefault()
+        setCategories(p => ([...p, newCategory]))
+        const updatedTasks = tasks.map(task => task.id === id ? { ...task, category: newCategory} : task)
+        setNewCategory('')
+        setTasks(updatedTasks)
+
+        const updatedTask = updatedTasks.find(t => t.id === id)
+        await updateTask(updatedTask)
+    }
+
+    const toggleDropdown = (id) => {
+        setDropdownOpen(prev => prev === id ? null : id)
+    }
+
+    return(
+        <div>
+        <button ref={buttonRef} onClick={() => toggleDropdown(task.id)} className={`dropdown-button mr-2 text-surface-a30 ${task.done ? '' : 'hover:text-primary-a0 cursor-pointer'} focus:outline-none transition`}>{task.category}</button>
+        {!task.done && 
+        <div ref={dropdownRef}
+        className={`dropdown mt-3 absolute -translate-x-4.5 rounded-xl bg-surface-a10 ring-1 ring-surface-a20 transition ${dropdownOpen === task.id ? "opacity-100 scale-100" : "opacity-0 scale-95 pointer-events-none"}`}
+            >
+            <ul className="text-surface-a50 select-none">
+            {categories.map((item, index) =>
+                <li key={index} onClick={() => handleCategory(task.id, item)} className={`my-2 mx-2 py-1 px-3 text-sm rounded-lg hover:text-white cursor-pointer transition ${task.category === item ? 'bg-primary-a0 text-white' : 'bg-surface-a20 '}`}>
+                {item} 
+                </li>
+            )}
+                <li key="new-collection" className="mx-2">
+                    <form onSubmit={(e) => handleAddCategory(e, task.id)}>
+                        <input
+                            placeholder="Add"
+                            className="mb-2 px-2 py-1 w-full rounded-lg text-center focus:outline-none field-sizing-content bg-surface-a20 placeholder:text-surface-a50 text-sm focus:placeholder:text-transparent transition"
+                            type="text"
+                            value={newCategory}
+                            onChange={(e) => setNewCategory(e.target.value)}
+                            />
+                    </form>
+                </li>
+            </ul>
+        </div>
+        }
+        </div>
+    )
+}
+
+export default DropdownCategories;
