fd7efa0ba123b2299f1dcc7990bd4c6b70c0b358
dima-kharchenko <dmitriy020606@gmail.com>
2025-06-09 23:25:00 +0200

    Add docker files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
+    plugins: [
         react(),
         tailwindcss(),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173
+    }
 })
